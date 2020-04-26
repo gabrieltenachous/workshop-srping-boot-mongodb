aa015b302061ac651b16c9f965f5573d0625248d
Gabriel Oliveira <gabriel_oliveira_f_sousa@outlook.com>
2020-04-26 17:10:44 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/nelioalves/workshopmongo/domain/Post.java
+++ src/main/java/com/nelioalves/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.nelioalves.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.nelioalves.workshopmongo.dto.AuthorDTO;
+import com.nelioalves.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -19,7 +22,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
-
+	private List<CommentDTO> comments = new ArrayList<>();
+	
 	public Post() {
 		// TODO Auto-generated constructor stub
 	}
@@ -96,6 +100,14 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comment) {
+		this.comments = comment;
 	}
 
 }
